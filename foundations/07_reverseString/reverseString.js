d4ebc52d83c901d8e1fa7a0cb92233e443962e9c
efra8ab <efra8ab@gmail.com>
2026-04-30 23:22:13 -0600

    exercise 7 complete

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = (str) => {
+    const arr = str.split('')
+        .reverse()
+        .join('')
+    return arr
+}
 
 // Do not edit below this line
 module.exports = reverseString;
