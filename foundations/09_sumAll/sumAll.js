38986faf080971b41d4db7a1f9441e9fc50bd582
efra8ab <efra8ab@gmail.com>
2026-04-30 23:48:31 -0600

    exercise 8 complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+    let total = 0
+    let smallest = 0
+    let biggest = 0
 
+    if (!Number.isInteger(a) || !Number.isInteger(b) ||
+        a < 0 || b < 0){
+        return 'ERROR'
+    }
+
+    if (a <= b) {
+        smallest = a
+        biggest = b
+    }
+    else {
+        smallest = b
+        biggest = a
+    }
+
+    for (smallest; smallest <= biggest; smallest++){
+        total += smallest
+    }
+    return total
 };
 
 // Do not edit below this line
