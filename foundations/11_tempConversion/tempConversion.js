833d8e9ee5bf36ad1ce98dff739ab075a4ba53a4
efra8ab <efra8ab@gmail.com>
2026-05-01 00:01:55 -0600

    exercise 11 complete

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = (deg) => {
+  let cel = (deg - 32) * (5 / 9)
+  return Math.round(cel * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (deg) => {
+  let far = (deg * (9 / 5) + 32)
+  return Math.round(far * 10) / 10
 };
 
 // Do not edit below this line
